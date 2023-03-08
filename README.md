2b6bd816370c074d651b149b08613b9055d57fcd
fenrir <darsha019@gmail.com>
2023-03-08 18:01:24 +0530

    A etch a sketch project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Etch-a-Sketch
+
+A Etch a sketch project made with html, css and vanilla js.
---
+++ script.js
@@ -0,0 +1,54 @@
+let grid = document.querySelector('.grid')
+let reset = document.getElementById('reset')
+let clear = document.getElementById('clear')
+let rainbow = document.getElementById('rainbow')
+let reload = document.getElementById('reload')
+
+reload.addEventListener('click', () => {
+    location.reload()
+})
+
+
+reset.addEventListener('click', () => {
+    let num = parseInt(prompt('select grid size number from 1-100'))
+    if(typeof(num) !== 'number') {
+        alert('ERROR')
+    }
+    else {
+        createGrid(num)
+    }
+})
+
+
+function createGrid(num) {
+    grid.style.gridTemplateRows = `repeat(${num}, 1fr)`
+    grid.style.gridTemplateColumns = `repeat(${num}, 1fr)`
+
+    for(let i = 0; i < num*num; i++) {
+        let cell = document.createElement('div')
+        cell.classList.add('square')
+        cell.style.backgroundColor = 'black'
+        cell.style.border = '1px solid lime'
+        grid.appendChild(cell)
+
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'white'
+        })
+        clear.addEventListener('click', () => {
+            cell.style.backgroundColor = 'black'
+        })
+
+        rainbow.addEventListener('click',() => {
+            cell.addEventListener('mouseover', () => {
+                let color = '#'
+                let arr = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+                for(let i = 0; i < 6; i++) {
+                    color += arr[Math.floor(Math.random()*arr.length)]
+                }
+                cell.style.backgroundColor = color
+            })
+        
+        })
+    }
+
+}
